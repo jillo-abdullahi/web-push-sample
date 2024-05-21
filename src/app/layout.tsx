2ce6223373eda63c75a93d76060667f0b768f6b2
jillo-abdullahi <jillo.abdulla@gmail.com>
2024-05-21 11:34:15 +0300

    chore: update name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Web push app",
+  title: "GasHawk Web Push",
   description: "A sample web push app",
   manifest: "/manifest.webmanifest"
 };
