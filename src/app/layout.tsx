de25f9f37f310d7f75ca91c2104b891e52906216
jillo-abdullahi <jillo.abdulla@gmail.com>
2024-05-21 11:17:16 +0300

    fix: add reference to manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Web push app",
   description: "A sample web push app",
+  manifest: "/manifest.webmanifest"
 };
 
 export default function RootLayout({
